e0e969447c60ca6487f26619b59731fdd95cf284
Thorben Luepkes <thorben.untamed93@gmail.com>
2018-03-25 18:05:10 +0200

    Final commit

--- WebDevCorp/ViewModels/ContactForm.cs
+++ WebDevCorp/ViewModels/ContactForm.cs
@@ -13,7 +13,7 @@ namespace WebDevCorp.ViewModels
         [Required(ErrorMessage = "Please enter email address")]
         [Display(Name = "Email")]
         [RegularExpression(@"^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$",
-            ErrorMessage = "Please enter correct email address")]
+        ErrorMessage = "Please enter correct email address")]
         public string Email { get; set; }
         [Required(ErrorMessage = "Please enter a subject")]
         public string Subject { get; set; }
